1ed747e1a9fed8fa18bc2f43b822761e9869d5af
jannamaher5-wq <jannamaher5@gmail.com>
2026-07-14 04:47:32 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        if (input.hasNext()) {
+            String s = input.next();
+            Greeter greeter = new Greeter(s);
+            greeter.sayHello();
+        }
+        
+        input.close();
+    }
+}
+
+class Greeter {
+    private String name;
+
+    public Greeter(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, " + name);
+    }
+}
