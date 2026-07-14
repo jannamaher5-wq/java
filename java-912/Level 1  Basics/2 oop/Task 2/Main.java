d994dc23903d8367aaad2f2301e036f6668b8ae2
jannamaher5-wq <jannamaher5@gmail.com>
2026-07-14 08:15:36 +0300

    Add files via upload

---
+++ java-912/Level 1  Basics/2 oop/Task 2/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Calculator {
+    private int a;
+    private char op;
+    private int b;
+
+    public Calculator(int num1, char operation, int num2) {
+        a = num1;
+        op = operation;
+        b = num2;
+    }
+
+    public int getResult() {
+        if (op == '+') {
+            return a + b;
+        } else if (op == '-') {
+            return a - b;
+        } else if (op == '*') {
+            return a * b;
+        } else {
+            return a / b;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        String[] parts = input.split("(?<=[-+*/])|(?=[-+*/])");
+
+        int num1 = Integer.parseInt(parts[0]);
+        char op = parts[1].charAt(0);
+        int num2 = Integer.parseInt(parts[2]);
+
+        Calculator calc = new Calculator(num1, op, num2);
+        System.out.println(calc.getResult());
+        
+        sc.close();
+    }
+}
