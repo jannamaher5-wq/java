fad797bb3f6893c51d75792e2fc28856eac1316c
jannamaher5-wq <jannamaher5@gmail.com>
2026-07-19 09:23:25 +0300

    Add files via upload

---
+++ java-912/Level 1  Basics/2 oop/Task 14/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextLong();
+        long k = scanner.nextLong();
+        long a = scanner.nextLong();
+
+        DataType dataType = new DataType(n, k, a);
+        dataType.check();
+    }
+}
+
+class DataType {
+    long n;
+    long k;
+    long a;
+
+    DataType(long n, long k, long a) {
+        this.n = n;
+        this.k = k;
+        this.a = a;
+    }
+
+    void check() {
+        long product = n * k;
+
+        if (product % a != 0) {
+            System.out.println("double");
+        } else {
+            long result = product / a;
+
+            if (result >= Integer.MIN_VALUE && result <= Integer.MAX_VALUE) {
+                System.out.println("int");
+            } else {
+                System.out.println("long long");
+            }
+        }
+    }
+}
---
+++ java-912/Level 1  Basics/2 oop/Task 15/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        LuckyNumber lucky = new LuckyNumber(n);
+        lucky.check();
+    }
+}
+
+class LuckyNumber {
+    int n;
+
+    LuckyNumber(int n) {
+        this.n = n;
+    }
+
+    void check() {
+        int first = n / 10;
+        int second = n % 10;
+
+        if ((second != 0 && first % second == 0) ||
+            (first != 0 && second % first == 0)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
