83d7d7a8e7c1e601331bbb32ade2667fcd2a6c2b
jannamaher5-wq <jannamaher5@gmail.com>
2026-07-21 09:16:20 +0300

    Update MemoAndMomo.java

--- java-912/Level 1  Basics/2 oop/Task 8/MemoAndMomo.java
+++ java-912/Level 1  Basics/2 oop/Task 8/MemoAndMomo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MemoAndMomo. {
+public class MemoAndMomo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
